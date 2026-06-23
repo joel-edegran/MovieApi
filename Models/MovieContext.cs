b48a0760e45994f696734636663dc9f89fbd7dae
Joel Edegran <joel_edegran@hotmail.com>
2026-06-23 14:07:40 +0200

    Add MovieContext base and register in Program.cs

---
+++ Models/MovieContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieApi.Models;
+
+public class MovieContext : DbContext
+{
+    public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+    { 
+    }
+}
