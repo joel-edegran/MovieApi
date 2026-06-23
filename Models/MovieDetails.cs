4b27809a6c3cdf6dabbc8311891161f29f86bacf
Joel Edegran <joel_edegran@hotmail.com>
2026-06-23 14:19:35 +0200

    Add Movie and MovieDetails domain models

---
+++ Models/MovieDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApi.Models;
+
+public class MovieDetails
+{
+    public int Id { get; set; }
+    public string Synopsis { get; set; } = string.Empty;
+    public string Language { get; set; } = string.Empty;
+    public decimal Budget { get; set; }
+}
