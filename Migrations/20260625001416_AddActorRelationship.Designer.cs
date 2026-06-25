8ff81906a146e5428e4391d7ed24e60dd90dc16d
Joel Edegran <joel_edegran@hotmail.com>
2026-06-25 16:19:34 +0200

    Add migration files for Actor relationship

--- Migrations/20260625001416_AddActorRelationship.Designer.cs
+++ Migrations/20260625001416_AddActorRelationship.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieApi.Models;
 
@@ -10,9 +11,11 @@ using MovieApi.Models;
 namespace MovieApi.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20260625001416_AddActorRelationship")]
+    partial class AddActorRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
